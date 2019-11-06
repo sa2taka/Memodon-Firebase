f228d9f38089393a9a70ec98ef5dbf70afcf36b5
sa2taka <sa2taka@gmail.com>
2019-11-06 05:02:10 +0000

    :wnrech: Adjust vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,22 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+interface RootState {
+  version: string;
+}
+
+const store = new Vuex.Store<RootState>({
+  state: {
+    version: '1.0.0',
+  },
+  plugins: [
+    createPersistedState({
+      key: 'memodonState',
+    }),
+  ],
 });
+
+export default store;
