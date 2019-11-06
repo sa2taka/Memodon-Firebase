fda13af1d9ab6c0685ee3247b85f3ab4acd6c69c
sa2taka <sa2taka@gmail.com>
2019-11-06 03:51:29 +0000

    :wrench: Add i18n

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+
+  pluginOptions: {
+    i18n: {
+      locale: "jp",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: true
+    }
+  }
 };
