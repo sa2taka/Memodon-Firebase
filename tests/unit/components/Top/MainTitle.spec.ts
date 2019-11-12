8e4ed478791442e642851ae3c4b5236f12bf7169
sa2taka <sa2taka@gmail.com>
2019-11-12 06:50:43 +0000

    :rotating_light: Fix to pass test

--- tests/unit/components/Top/MainTitle.spec.ts
+++ tests/unit/components/Top/MainTitle.spec.ts
@@ -2,11 +2,14 @@ import { shallowMount, createLocalVue } from '@vue/test-utils';
 import MainTitle from '@/components/Top/MainTitle.vue';
 import Vuex from 'vuex';
 import Vuetify from 'vuetify';
+import i18n from '@/i18n';
+import VueI18n from 'vue-i18n';
 
 const localVue = createLocalVue();
 
 localVue.use(Vuex);
 localVue.use(Vuetify);
+localVue.use(VueI18n);
 
 describe('Component', () => {
   test('is a Vue instance', () => {
@@ -17,7 +20,7 @@ describe('Component', () => {
         },
       },
     });
-    const wrapper = shallowMount(MainTitle, { store, localVue });
+    const wrapper = shallowMount(MainTitle, { store, i18n, localVue });
     expect(wrapper.element).toMatchSnapshot();
   });
 });
