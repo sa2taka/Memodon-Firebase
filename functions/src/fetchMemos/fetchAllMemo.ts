c4a141ebffa7f67d6895b6bf88357b0ce824bde9
sa2taka <sa2taka@gmail.com>
2019-11-12 08:44:20 +0000

    Add to prepare fetch memo

---
+++ functions/src/fetchMemos/fetchAllMemo.ts
@@ -0,0 +1,8 @@
+import { firestore } from 'firebase-admin';
+import { consumerKey, consumerSecret } from '../secrets/twitter';
+
+function fetchMemoFromTwitter(token: string, secret: string) {}
+
+function fetchAllMemos(snapshot: firestore.DocumentSnapshot) {}
+
+function fetchUserFromFireStore(uuid: string) {}
