ef7e8f62604dfae8a74ad45e6ee8f6571f32c8ac
sa2taka <sa2taka@gmail.com>
2019-11-14 08:08:59 +0000

    :sparkles: Add router setting

---
+++ src/views/Note.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container justify="center" mt-9>note</v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component({
+  components: {},
+})
+export default class Note extends Vue {}
+</script>
+
+<style lang="scss"></style>
