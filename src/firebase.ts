ac62f2caf8f917351596a1b8a17fd165cf6b84de
sa2taka <sa2taka@gmail.com>
2019-11-14 08:23:43 +0000

    :wrench: Add to cache data

--- src/firebase.ts
+++ src/firebase.ts
@@ -15,4 +15,20 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+// enable cache
+firebase
+  .firestore()
+  .enablePersistence()
+  .catch(function(err) {
+    if (err.code == 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+      // ...
+    } else if (err.code == 'unimplemented') {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+    }
+  });
+
 export default firebase;
