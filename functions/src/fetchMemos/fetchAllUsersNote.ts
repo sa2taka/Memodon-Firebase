60ef000f006f98e9aa998bbe167810096a3b8ae7
sa2taka <sa2taka@gmail.com>
2019-12-05 04:44:53 +0000

    :bug: Fix to save note

--- functions/src/fetchMemos/fetchAllUsersNote.ts
+++ functions/src/fetchMemos/fetchAllUsersNote.ts
@@ -1,9 +1,10 @@
 import { firestore } from 'firebase-admin';
-import fetchTwitterMemo from './fetchTwitterMemo';
+
+import updateNote from '../firestore/updateNote';
 
 export function fetchAllUsersNote(userSnapshot: firestore.DocumentSnapshot) {
   // const subUserCollecgion = snapshot.ref.collection('subUsers');
-  const promises = [fetchTwitterMemoFromSnap(userSnapshot)];
+  const promises = [updateNote(userSnapshot)];
 
   return Promise.all(promises);
 }
@@ -16,43 +17,4 @@ export function fetchUserFromFireStore(uuid: string) {
     .then((snap: firestore.DocumentSnapshot) => {
       return fetchAllUsersNote(snap);
     });
-}
-
-function fetchTwitterMemoFromSnap(userSnapshot: firestore.DocumentSnapshot) {
-  const userData = userSnapshot.data();
-  if (!userData) {
-    return;
-  }
-
-  const secretsPromise = userSnapshot.ref
-    .collection('secrets')
-    .doc('twitter.com')
-    .get();
-
-  const memosPromise = userSnapshot.ref
-    .collection('memos')
-    .orderBy('id', 'desc')
-    .limit(1)
-    .get();
-
-  return Promise.all([secretsPromise, memosPromise]).then((resolved) => {
-    const [twitterSecretsSnap, memosSnap] = resolved;
-    const secretData = twitterSecretsSnap.data();
-    const latestMemoDoc = memosSnap.docs[0];
-    let latestMemoId: string;
-
-    if (latestMemoDoc) {
-      latestMemoId = latestMemoDoc.data().id.toString();
-    } else {
-      latestMemoId = '0';
-    }
-
-    if (!secretData || !latestMemoId) {
-      return;
-    }
-
-    const { token, secret } = secretData[userData.twitterId];
-
-    return fetchTwitterMemo(userData.twitterId, token, secret, latestMemoId);
-  });
 }
