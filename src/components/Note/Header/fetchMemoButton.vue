5f86278178655842135182898ec67f0423604c0a
sa2taka <sa2taka@gmail.com>
2019-12-05 09:12:23 +0000

    :art: Add animation to fetch memo button

--- src/components/Note/Header/fetchMemoButton.vue
+++ src/components/Note/Header/fetchMemoButton.vue
@@ -1,12 +1,13 @@
 <template>
   <v-btn color="secondary" @click="callToFetchMemo" width="160">
     <transition name="fade">
-      <p v-show="!isLoading" class="my-auto">
-        <v-icon small class="mr-2">fa-redo</v-icon>{{ $t('updateMemo') }}
+      <p v-show="!isLoading" class="my-auto fade">
+        <v-icon small class="mr-2">fa-redo</v-icon>
+        {{ $t('updateMemo') }}
       </p>
     </transition>
     <transition name="fade">
-      <p v-show="isLoading" class="my-auto">
+      <p v-show="isLoading" class="my-auto fade">
         <loading :side="30"></loading>
       </p>
     </transition>
@@ -37,7 +38,19 @@ export default class fetchMemoButton extends Vue {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.fade {
+  transition: all 0.3s;
+  &-enter,
+  &-leave-to {
+    opacity: 0;
+  }
+
+  &-leave-active {
+    position: absolute;
+  }
+}
+</style>
 
 <i18n>
 {
