6cd20812d0d88629212be77a549d5396b3e94c6a
sa2taka <sa2taka@gmail.com>
2019-12-05 09:07:55 +0900

    :sparkles: Add fetch memo

--- functions/src/fetchMemos/fetchAllUsersNote.ts
+++ functions/src/fetchMemos/fetchAllUsersNote.ts
@@ -1,9 +1,58 @@
 import { firestore } from 'firebase-admin';
+import fetchTwitterMemo from './fetchTwitterMemo';
 
-export function fetchAllUsersNote(snapshot: firestore.DocumentSnapshot) {
-  // TODO fetch twitter and users note
+export function fetchAllUsersNote(userSnapshot: firestore.DocumentSnapshot) {
+  // const subUserCollecgion = snapshot.ref.collection('subUsers');
+  const promises = [fetchTwitterMemoFromSnap(userSnapshot)];
+
+  return Promise.all(promises);
 }
 
 export function fetchUserFromFireStore(uuid: string) {
-  // TODO fetch user snapshot from uuid
+  return firestore()
+    .collection('users')
+    .doc(uuid)
+    .get()
+    .then((snap: firestore.DocumentSnapshot) => {
+      return fetchAllUsersNote(snap);
+    });
+}
+
+function fetchTwitterMemoFromSnap(userSnapshot: firestore.DocumentSnapshot) {
+  const userData = userSnapshot.data();
+  if (!userData) {
+    return;
+  }
+
+  const secretsPromise = userSnapshot.ref
+    .collection('secrets')
+    .doc('twitter.com')
+    .get();
+
+  const memosPromise = userSnapshot.ref
+    .collection('memos')
+    .orderBy('id', 'desc')
+    .limit(1)
+    .get();
+
+  return Promise.all([secretsPromise, memosPromise]).then((resolved) => {
+    const [twitterSecretsSnap, memosSnap] = resolved;
+    const secretData = twitterSecretsSnap.data();
+    const latestMemoDoc = memosSnap.docs[0];
+    let latestMemoId: string;
+
+    if (latestMemoDoc) {
+      latestMemoId = latestMemoDoc.data().id.toString();
+    } else {
+      latestMemoId = '0';
+    }
+
+    if (!secretData || !latestMemoId) {
+      return;
+    }
+
+    const { token, secret } = secretData[userData.twitterId];
+
+    return fetchTwitterMemo(userData.twitterId, token, secret, latestMemoId);
+  });
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,5 +2,7 @@ import * as admin from 'firebase-admin';
 admin.initializeApp();
 
 import * as User from './subscribe/firestore/user';
+import * as FetchMemo from './subscribe/https/fetchMemo';
 
 exports.onCreateUser = User.onCreate;
+exports.onCallFetchMemo = FetchMemo.fetchMemo;
