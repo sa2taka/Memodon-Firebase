7b4e4f993eca10bc74242a8001f40474c78cabd6
sa2taka <sa2taka@gmail.com>
2019-12-06 07:33:12 +0000

    :sparkles: Add to disable the button if it was within 15 minutes from the last fetch time

--- src/components/Note/Header/fetchMemoButton.vue
+++ src/components/Note/Header/fetchMemoButton.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-btn color="secondary" @click="callToFetchMemo" width="160">
+  <v-btn
+    color="secondary"
+    @click="callToFetchMemo"
+    :disabled="disable"
+    width="160"
+  >
     <transition name="fade">
       <p v-show="!isLoading" class="my-auto fade">
         <v-icon small class="mr-2">fa-redo</v-icon>
@@ -18,6 +23,8 @@
 import { Component, Vue } from 'vue-property-decorator';
 import firebase from '@/firebase';
 
+import User from '@/store/modules/user';
+
 import Loading from '@/components/Atomic/Loading.vue';
 
 @Component({
@@ -27,13 +34,61 @@ import Loading from '@/components/Atomic/Loading.vue';
 })
 export default class fetchMemoButton extends Vue {
   public isLoading = false;
+  public fifteenMinutesAfterlastFetchDay: number = 0;
+  public disable = true;
+
+  public created() {
+    this.subscribeUserAndUpdateDisable();
+    this.startDisableUpdateCycle();
+  }
+
   public callToFetchMemo() {
     const fetchMemo = firebase.functions().httpsCallable('fetchMemo');
     this.isLoading = true;
+    this.disable = false;
 
     fetchMemo().then((result) => {
       this.isLoading = false;
+      this.updateDisableStatus();
     });
+  }
+
+  private subscribeUserAndUpdateDisable() {
+    const currentUser = firebase.auth().currentUser || User;
+    const callback = (uid: string) => {
+      firebase
+        .firestore()
+        .collection('users')
+        .doc(uid)
+        .onSnapshot((user) => {
+          const data = user.data();
+
+          if (data) {
+            const lastFetchDay: Date = data.lastFetched.toDate();
+            this.fifteenMinutesAfterlastFetchDay =
+              lastFetchDay.getTime() + 15 * 60 * 1000;
+            this.updateDisableStatus();
+          }
+        });
+    };
+
+    if (currentUser.uid === '') {
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          callback(user.uid);
+        }
+      });
+    } else {
+      callback(currentUser.uid);
+    }
+  }
+
+  private startDisableUpdateCycle() {
+    setInterval(this.updateDisableStatus, 3 * 60 * 1000);
+  }
+
+  private updateDisableStatus() {
+    this.disable = Date.now() < this.fifteenMinutesAfterlastFetchDay;
   }
 }
 </script>
